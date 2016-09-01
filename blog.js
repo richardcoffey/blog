1ff027943770c923e61896d2beefa9f775f04dfd
Patrick Coffey <patrickcoffey48@gmail.com>
2016-09-01 16:45:10 -0500

    Basic web app

--- blog.js
+++ blog.js
@@ -3,4 +3,19 @@
  * Main file that handles routing for this blog.
  */
 
+// Require dependencies.
+const express = require('express');
 
+// Create express app.
+const app = express();
+
+// Set details for this website.
+app.locals.title = 'Richard Coffey';
+app.locals.email = 'richardcoffey32@gmail.com';
+
+// Create home endpoint.
+app.get('/', (request, result) => {
+  result.send('Hello World!');
+});
+
+app.listen(3000);
